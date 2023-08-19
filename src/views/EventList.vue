1f61daee9642d8be98abecb3453d888c43c4a676
luoar rian <rian.luoar@gmail.com>
2023-08-19 23:56:46 +0530

    pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,31 +2,52 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <router-link :to="{ name: 'EventList', query: { page: page - 1 } }" v-if="page != 0">
+      Prev Page
+    </router-link>
+
+    <router-link :to="{ name: 'EventList', query: { page: page + 1 } }" v-if="hasNextPage">
+      Next Page
+    </router-link>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
 
 export default {
   name: 'EventList',
+  props: ['page'],
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0
     }
   },
   created() {
-    EventService.getEvents()
+    watchEffect(()=>{
+      this.events = null
+      EventService.getEvents(2, this.page)
       .then(response => {
         this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
       })
       .catch(error => {
         console.log(error)
       })
+    })
+  },
+  computed: {
+    hasNextPage(){
+      const totalPages = Math.ceil(this.totalEvents/2)
+      return this.page < totalPages
+    }
   }
 }
 </script>
