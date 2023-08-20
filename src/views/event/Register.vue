5ce12a2985467faee945d32a9761b79eb611dac0
luoar rian <rian.luoar@gmail.com>
2023-08-20 11:17:36 +0530

    programmatic calling router

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,18 @@
 <template>
-  <p>Register for the event here</p>
+    <p>Register for the event here</p>
+    <button @click="register">Register</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+    props: ['event'],
+    methods: {
+        register() {
+            this.$router.push({
+                name: 'EventDetails',
+                params: { id: this.event.id }
+            })
+        }
+    }
 }
 </script>
